0f5c686f0a8b262e45a6b27991ba3159d602d678
L.Blondy <51233631+L-Blondy@users.noreply.github.com>
2019-11-11 12:13:07 +0100

    try / catch

--- src/slideIn.js
+++ src/slideIn.js
@@ -2,37 +2,41 @@ import "./slideIn.css";
 
 export default function slideInView ( nodeList, { from = "bottom", duration = 700, delay = 0, reverse = false } = {} ) {
 
-	if ( !nodeList.length )
-		nodeList = [ nodeList ]
-	if ( reverse ) {
-		nodeList = [].slice.call( nodeList ).reverse()
-	}
-	function slideCb ( entries ) {
-		entries.forEach( ( entry, index ) => {
-			if ( entry.isIntersecting || from === "top" ) {
-				setTimeout( () => entry.target.classList += " slide-in ", delay * index )
+	try {
+		if ( !nodeList.length )
+			nodeList = [ nodeList ]
+		if ( reverse ) {
+			nodeList = [].slice.call( nodeList ).reverse()
+		}
+		function slideCb ( entries ) {
+			entries.forEach( ( entry, index ) => {
+				if ( entry.isIntersecting || from === "top" ) {
+					setTimeout( () => entry.target.classList += " slide-in ", delay * index )
+				}
+			} )
+		}
+
+		const slideObs = new IntersectionObserver( slideCb, {} );
+
+		[].forEach.call( nodeList, elem => {
+			slideObs.observe( elem );
+			elem.style.animationDuration = duration + "ms";
+
+			switch ( from ) {
+				case "bottom":
+					return elem.classList += " pre-slide-in-bottom "
+				case "top":
+					return elem.classList += " pre-slide-in-top "
+				case "left":
+					return elem.classList += " pre-slide-in-left "
+				case "right":
+					return elem.classList += " pre-slide-in-right "
+				default:
+					return elem.classList += " pre-slide-in-bottom "
 			}
 		} )
 	}
-
-	const slideObs = new IntersectionObserver( slideCb, {} );
-
-	[].forEach.call( nodeList, elem => {
-		slideObs.observe( elem );
-		elem.style.animationDuration = duration + "ms";
-
-		switch ( from ) {
-			case "bottom":
-				return elem.classList += " pre-slide-in-bottom "
-			case "top":
-				return elem.classList += " pre-slide-in-top "
-			case "left":
-				return elem.classList += " pre-slide-in-left "
-			case "right":
-				return elem.classList += " pre-slide-in-right "
-			default:
-				return elem.classList += " pre-slide-in-bottom "
-		}
-
-	} )
+	catch ( error ) {
+		throw new Error( "No slide in animation, this browser might not support the IntersectionObsever API" )
+	}
 }
