5d3763ec1f621d61b6bf0559bb5583b728c0bcf4
Laurent Blondy <laurent.blondy1@gmail.com>
2019-11-11 10:19:57 +0100

    created scripts

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+import "lb-lazy-images";
 import setScrollNav from "lb-scroll-nav";
-import slideInView from "./slideInView";
+import slideInView from "./slideIn";
 import "./styles.css";
 
 /************** NAVBAR ***************/
@@ -9,14 +10,20 @@ setScrollNav( sections, sectionsContainer )
 
 /************ SLIDE IN VIEW ************/
 
-const images = document.querySelector( ".images" ).children;
+const images = document.querySelector( ".images" );
 const images_options = {
 	from: "left",
-	delay: 130,
+}
+slideInView( images, images_options );
+
+const images3 = document.querySelector( ".images" ).children;
+const images3_options = {
+	from: "bottom",
+	delay: 200,
 	duration: "1000ms",
 	reverse: false
 }
-slideInView( images, images_options );
+slideInView( images3, images3_options );
 
 const navlinks = document.querySelectorAll( ".navlink" );
 const navlinks_options = {
